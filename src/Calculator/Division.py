0005892e97cb008d7bc179202b69d3436d06f792
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-08 17:23:51 -0500

    Fix: Fixed the code format.

--- src/Calculator/Division.py
+++ src/Calculator/Division.py
@@ -1,4 +1,4 @@
 def division(num1, num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return round(num2 / num1)
+    num1 = float(num1)
+    num2 = float(num2)
+    return round(num2 / num1, 7)
