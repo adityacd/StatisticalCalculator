9c3abcb1b26e2fe48a57236c1caca44ca9197cb2
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-08 16:46:33 -0500

    Feature: Created the Variance function.

---
+++ src/Statistics/Variance.py
@@ -0,0 +1,6 @@
+from src.Calculator.Square import square
+from src.Statistics.StandardDeviation import standard_deviation
+
+
+def variance(numbers):  # complete
+    return square(standard_deviation(numbers))
