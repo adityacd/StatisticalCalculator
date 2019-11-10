d29a24aa3a63ae3cad25634625b666f50564f80f
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-10 16:05:33 -0500

    Fix: Loop issue solved and updated

--- CSVReading/CSVReading.py
+++ CSVReading/CSVReading.py
@@ -12,14 +12,12 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-        try:
-            with open(absolutepath(filepath)) as text_data:
-                csv_data = csv.DictReader(text_data, delimiter=',')
-                for row in csv_data:
-                    self.data.append(row)
-        except OSError:
-            print('cannot open', filepath)
-        # pprint(self.data)
+
+        with open(absolutepath(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+        pass
 
     def return_data_as_objects(self, class_name):
         objects = []
