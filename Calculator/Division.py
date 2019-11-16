d0f354d7b49a289249f41db024fa0a685781d680
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-16 00:47:57 -0500

    Task: Added exceptions for infinite value.

--- Calculator/Division.py
+++ Calculator/Division.py
@@ -1,4 +1,7 @@
 def division(num1, num2):
-    num1 = float(num1)
-    num2 = float(num2)
-    return round(num2 / num1, 7)
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        return round(num2 / num1, 7)
+    except:
+        print('Division by zero not possible')
