92001becc56a55f0d6d613d6f2af41210520bdc5
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-16 00:47:38 -0500

    Task: Added exceptions for file not found.

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -15,66 +15,96 @@ class MyTestCase(unittest.TestCase):
 
     def test_addition_method_calculator(self):  # Testing of the add function
         print("Testing Addition")
-        test_data = CsvReader('./Data/Unit Test Addition.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Addition.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = row['Value 1']
             y = row['Value 2']
             expect_result = int(row['Result'])
             self.assertEqual(self.calobject.add(x, y), expect_result)
             self.assertEqual(self.calobject.result, expect_result)
+        print('Successful Testing!')
 
     def test_subtraction_method_calculator(self):  # Testing of the subtract function
         print(' ')
         print('Testing Subtraction')
-        test_data = CsvReader('./Data/Unit Test Subtraction.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Subtraction.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = row['Value 1']
             y = row['Value 2']
             expect_result = int(row['Result'])
             self.assertEqual(self.calobject.subtract(x, y), expect_result)
             self.assertEqual(self.calobject.result, expect_result)
+        print('Successful Testing!')
 
     def test_multiply_method_calculator(self):  # Testing of the multiply function
         print(' ')
         print('Testing Multiplication')
-        test_data = CsvReader('./Data/Unit Test Multiplication.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Multiplication.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = int(row['Value 1'])
             y = int(row['Value 2'])
             expect_result = int(row['Result'])
             self.assertEqual(self.calobject.multiply(x, y), expect_result)
             self.assertEqual(self.calobject.result, expect_result)
+        print('Successful Testing!')
 
     def test_divide_method_calculator(self):  # Testing of the divide function
         print(' ')
         print('Testing Division')
-        test_data = CsvReader('./Data/Unit Test Division.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Division.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = float(row['Value 1'])
             y = float(row['Value 2'])
             expect_result = float(row['Result'])
             self.assertEqual(self.calobject.divide(x, y), round(expect_result, 7))
             self.assertEqual(self.calobject.result, round(expect_result, 7))
+        print('Successful Testing!')
 
     def test_square_method_calculator(self):  # Testing of the square function
         print(' ')
         print('Testing Squares')
-        test_data = CsvReader('./Data/Unit Test Square.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Square.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = int(row['Value 1'])
             expect_result = int(row['Result'])
             self.assertEqual(self.calobject.square(x), expect_result)
             self.assertEqual(self.calobject.result, expect_result)
+        print('Successful Testing!')
 
     def test_sqrt_method_calculator(self):  # Testing of the square_root function
         print(' ')
         print('Testing Square Roots')
-        test_data = CsvReader('./Data/Unit Test Square Root.csv').data  # Loading the .csv data file
+        try:
+            test_data = CsvReader('./Data/Unit Test Square Root.csv').data  # Loading the .csv data file
+        except:
+            print('File not found, Please input valid file.')
+
         for row in test_data:
             x = float(row['Value 1'])
             expect_result = float(row['Result'])
             self.assertEqual(self.calobject.square_root(x), round(expect_result, 8))
             self.assertEqual(self.calobject.result, round(expect_result, 8))
+        print('Successful Testing!')
 
 
 if __name__ == '__main__':  # This runs the unittest
--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -16,8 +16,12 @@ class MyTestCase(unittest.TestCase):
     def test_Population_Mean_calculator(self):  # complete
         print(' ')
         print('Testing Population Mean')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             a = int(row['Value 1'])
@@ -26,12 +30,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['Mean']))
         self.assertEqual(self.statobj.population_mean(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_Median_calculator(self):  # complete
         print(' ')
         print('Testing Population Median')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -40,12 +49,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['Median']))
         self.assertEqual(self.statobj.population_median(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_Mode_calculator(self):  # complete
         print(' ')
         print('Testing Population Mode')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -54,12 +68,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['Mode']))
         self.assertEqual(self.statobj.population_mode(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_Population_Standard_Deviation_calculator(self):  # complete
         print(' ')
         print('Testing Population Standard Deviation')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -68,12 +87,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['StanDeviation']))
         self.assertEqual(self.statobj.population_standard_deviation(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_Population_Variance_calculator(self):  # complete
         print(' ')
         print('Testing Population Variance')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -82,11 +106,16 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['Variance']))
         self.assertEqual(self.statobj.population_variance(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_Sample_Standard_Deviation_calculator(self):  # complete
         print(' ')
         print('Testing Sample Standard Deviation')
-        test_data = CsvReader('Data/StatData.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -95,23 +124,33 @@ class MyTestCase(unittest.TestCase):
         x = round(x, 3)
         z = round(z, 3)
         self.assertEqual(x, z)
+        print('Successful Testing!')
 
     def test_Sample_Mean_calculator(self):  # complete
         print(' ')
         print('Testing Sample Mean')
-        test_data = CsvReader('Data/StatData.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
             dataset.append(y)
         x, z = self.statobj.samplemean(dataset)
         self.assertEqual(x, z)
+        print('Successful Testing!')
 
     def test_z_score_calculator(self):  # complete
         print(' ')
         print('Testing ZScore')
-        test_data = CsvReader('Data/StatData.csv').data
-        data_answer = CsvReader('Data/Zscore.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            data_answer = CsvReader('Data/Zscore.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -122,13 +161,18 @@ class MyTestCase(unittest.TestCase):
             data_answer1.append(z)
         self.assertEqual(self.statobj.z_score(dataset), data_answer1)
         self.assertEqual(self.statobj.result, data_answer1)
+        print('Successful Testing!')
 
     def test_population_correlation_coefficient(self):  # complete
         print(' ')
         print('Testing Population Correlation Coefficient')
-        test_data = CsvReader('Data/StatData.csv').data
-        test_data2 = CsvReader('Data/data2.csv').data
-        ans = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            test_data2 = CsvReader('Data/data2.csv').data
+            ans = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -141,12 +185,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float(row['Pop_corrcoef'])
         self.assertEqual(self.statobj.population_correlation_coefficient(dataset, dataset2), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_proportion_calculator(self):  # complete
         print(' ')
         print('Testing Proportion')
-        test_data = CsvReader('Data/StatData.csv').data
-        answer = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            answer = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
@@ -155,12 +204,17 @@ class MyTestCase(unittest.TestCase):
             expect_result = float((column['Proportion']))
         self.assertEqual(self.statobj.proportion(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     def test_confidence_interval(self):  #complete
         print(' ')
         print('Testing Confidence Interval')
-        test_data = CsvReader('Data/StatData.csv').data
-        ans = CsvReader('Data/confidenceresult.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            ans = CsvReader('Data/confidenceresult.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             x = int(row['Value 1'])
@@ -170,12 +224,17 @@ class MyTestCase(unittest.TestCase):
             y = float(row['Result'])
             expect_result.append(y)
         self.assertEqual(self.statobj.population_confidence_interval(dataset), expect_result)
+        print('Successful Testing!')
 
     def test_variance_of_population_proportion(self): #complete
         print(' ')
         print('Testing Variance of Population Proportion')
-        test_data = CsvReader('Data/StatData.csv').data
-        ans = CsvReader('Data/StatDataAnswers.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+            ans = CsvReader('Data/StatDataAnswers.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             x = int(row['Value 1'])
@@ -184,6 +243,7 @@ class MyTestCase(unittest.TestCase):
             expect_result = float(row['Var_pop_prop'])
         self.assertEqual(self.statobj.variance_of_population_proportion(dataset), expect_result)
         self.assertEqual(self.statobj.result, expect_result)
+        print('Successful Testing!')
 
     #def test_p_value(self):
      #   print(' ')
@@ -199,13 +259,18 @@ class MyTestCase(unittest.TestCase):
     def test_Variance_of_Sample_Proportion(self): #complete
         print(' ')
         print('Testing Variance of Sample Proportion')
-        test_data = CsvReader('Data/StatData.csv').data
+        try:
+            test_data = CsvReader('Data/StatData.csv').data
+        except:
+            print('File not found, Please input valid file.')
+
         dataset = []
         for row in test_data:
             y = int(row['Value 1'])
             dataset.append(y)
         x = self.statobj.variance_of_samp_proportion(dataset)
         self.assertEqual(x, x)
+        print('Successful Testing!')
 
 
 if __name__ == '__main__':  # This runs the unittest
