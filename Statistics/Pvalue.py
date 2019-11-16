aac281da57fcf464530fe9409d2af510e9771dee
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-11-15 23:35:32 -0500

    Feature: Created the p Value

---
+++ Statistics/Pvalue.py
@@ -0,0 +1,11 @@
+from Statistics.ZScore import zscore
+
+
+def p_value(numbers):
+    x = []
+    n = len(numbers)
+    z = zscore(numbers)
+    for i in y:
+        if i == z:
+            x.append(y)
+    return y
