ac72aa33a0ce963c556658f197082299a9ea2de5
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-06 15:15:05 -0500

    Feature: Created the Stuntman function

--- Database/new_sql_hw.py
+++ Database/new_sql_hw.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey, Boolean
+from sqlalchemy.orm import relationship, backref
 
 engine = create_engine('sqlite:////web/Sqlite-Data/Movies_Database.db')
 Session = sessionmaker(bind=engine)
@@ -39,3 +39,17 @@ class Actor(Base):
     def __init__(self, name, birthday):
         self.name = name
         self.birthday = birthday
+
+class Stuntman(Base):
+    __tablename__ = 'stuntmen'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    active = Column(Boolean)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship("Actor", backref=backref("stuntman", uselist=False))
+
+    def __init__(self, name, active, actor):
+        self.name = name
+        self.active = active
+        self.actor = actor
