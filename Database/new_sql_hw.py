9664673aad9cbabb785ba1f6f22a0248204ee7e9
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-06 15:15:39 -0500

    Feature: Created ContactDetails function

--- Database/new_sql_hw.py
+++ Database/new_sql_hw.py
@@ -53,3 +53,17 @@ class Stuntman(Base):
         self.name = name
         self.active = active
         self.actor = actor
+
+class ContactDetails(Base):
+    __tablename__ = 'contact_details'
+
+    id = Column(Integer, primary_key=True)
+    phone_number = Column(String)
+    address = Column(String)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship("Actor", backref="contact_details")
+
+    def __init__(self, phone_number, address, actor):
+        self.phone_number = phone_number
+        self.address = address
+        self.actor = actor
