34e5e1e33ecddf46dfd068dd13f03c345976f578
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-06 15:19:29 -0500

    Feature: Added the data to the functions

--- Database/new_sql_hw.py
+++ Database/new_sql_hw.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey, Boolean
 from sqlalchemy.orm import relationship, backref
+from datetime import date
 
 engine = create_engine('sqlite:////web/Sqlite-Data/Movies_Database.db')
 Session = sessionmaker(bind=engine)
@@ -67,3 +68,49 @@ class ContactDetails(Base):
         self.phone_number = phone_number
         self.address = address
         self.actor = actor
+
+
+Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# 4 - create movies
+bourne_identity = Movie("The Bourne Identity", date(2002, 10, 11))
+furious_7 = Movie("Furious 7", date(2015, 4, 2))
+pain_and_gain = Movie("Pain & Gain", date(2013, 8, 23))
+
+# 5 - creates actors
+matt_damon = Actor("Matt Damon", date(1970, 10, 8))
+dwayne_johnson = Actor("Dwayne Johnson", date(1972, 5, 2))
+mark_wahlberg = Actor("Mark Wahlberg", date(1971, 6, 5))
+
+# 7 - add contact details to actors
+matt_contact = ContactDetails("415 555 2671", "Burbank, CA", matt_damon)
+dwayne_contact = ContactDetails("423 555 5623", "Glendale, CA", dwayne_johnson)
+dwayne_contact_2 = ContactDetails("421 444 2323", "West Hollywood, CA", dwayne_johnson)
+mark_contact = ContactDetails("421 333 9428", "Glendale, CA", mark_wahlberg)
+
+# 8 - create stuntmen
+matt_stuntman = Stuntman("John Doe", True, matt_damon)
+dwayne_stuntman = Stuntman("John Roe", True, dwayne_johnson)
+mark_stuntman = Stuntman("Richard Roe", True, mark_wahlberg)
+
+# 9 - persists data
+session.add(bourne_identity)
+session.add(furious_7)
+session.add(pain_and_gain)
+
+session.add(matt_contact)
+session.add(dwayne_contact)
+session.add(dwayne_contact_2)
+session.add(mark_contact)
+
+session.add(matt_stuntman)
+session.add(dwayne_stuntman)
+session.add(mark_stuntman)
+
+# 10 - commit and close session
+session.commit()
