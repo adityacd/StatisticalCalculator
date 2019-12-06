3387b2688e5fa81847a79f4aba6f6d67466f8083
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-06 15:05:02 -0500

    Feature: Added Movies function

--- Database/new_sql_hw.py
+++ Database/new_sql_hw.py
@@ -1,8 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, Date
 
 engine = create_engine('sqlite:////web/Sqlite-Data/Movies_Database.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
